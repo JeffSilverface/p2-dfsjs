601a5a89f52439310c7848c8818e7c0aa682d236
Jean-François PANN <78153059+JeffSilverface@users.noreply.github.com>
2026-04-17 09:01:56 +0200

    Fix: gestion de la réduction de la taille d'écran en affichant moins de texte

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,8 +1,11 @@
 export const HeaderComponent = () => {
   return (
     <div className="bg-gray-800 p-4">
-      <h1 className="text-2xl md:text-4xl font-bold">
-        Historique des Jeux Olympiques - TéléSport
+      <h1 className="text-xl md:text-3xl font-bold">
+        Historique des&nbsp;
+        <span className="hidden md:inline">Jeux Olympiques</span>
+        <span className="md:hidden">J.O.</span>
+        &nbsp;- TéléSport
       </h1>
     </div>
   );
